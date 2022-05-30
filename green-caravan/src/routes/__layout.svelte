e79d15e8ffca11994fb8db527f67d9fb58ac5446
zainuwachtig <zain.butt@hva.nl>
2022-05-30 21:28:12 +0200

    initial commit

---
+++ green-caravan/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../styles/style.css';
+</script>
+
+<header>
+	<img src="/green-caravan-logo.svg" alt="" />
+</header>
+<main>
+	<slot />
+</main>
+<footer>
+	<p>The Green Caravan & Zain</p>
+</footer>
---
+++ green-caravan/src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+	let name = 'pakoe';
+</script>
+
+<h1>Welcome {name}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
