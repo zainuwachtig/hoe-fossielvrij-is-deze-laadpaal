5a7e6488a20d9f11a1284ee917469737098c90a1
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-06-20 16:21:48 +0200

    Vergelijking pagina verbeterd

--- green-caravan/src/lib/comparison.svelte
+++ green-caravan/src/lib/comparison.svelte
@@ -5,6 +5,8 @@
 	export let itemName;
 	export let itemAmount;
 	export let itemEmoji;
+	export let nextPage;
+	export let button
 
 	let item;
 
@@ -30,22 +32,35 @@
 		item.classList.add('vergelijking-tonen');
 	};
 
-	letItRain = () => {
+	const disableButton = () => {
+		button.style.opacity = 0
+		button.style.visibility = 'hidden'
+
+		setTimeout(enableButton, 25000)
+	}
+
+	const enableButton = () => {
+		button.style.opacity = 1
+		button.style.visibility = 'visible'
+	}
+
+	 letItRain = () => {
+		disableButton()
 		for (let index = 0; index < itemAmount; index++) {
 			createItem();
 		}
 	};
 </script>
 
-<div class="vergelijking" bind:this={item} on:click={letItRain}>
-	<p>15.000g CO2 is</p>
+<div id="{itemEmoji}" class="vergelijking" bind:this={item}>
 	<h2><span>{itemAmount}</span> {itemName}</h2>
+	<p>15kg CO2</p>
+	<button bind:this={button} on:click|preventDefault={letItRain}>Visualiseer</button>
+	<a class="material-symbols-outlined right" href='#{nextPage}'>arrow_right_alt</a>
 </div>
 
 <style>
 	.vergelijking {
-		color: white;
-		/* opacity: 0; */
 		flex: 0 0 auto;
 		width: 100%;
 		scroll-snap-align: center;
@@ -53,8 +68,47 @@
 		position: relative;
 	}
 
-	.vergelijking h2 {
-		z-index: 10;
+	.vergelijking p {
+		color: var(--text-color);
 		position: relative;
+		z-index: 100;
+		text-shadow: 0 0 5px #fff;
+	}
+
+	.vergelijking h2 {
+		margin-top: 10rem;
+		position: relative;
+		z-index: 100;
+		text-shadow: 0 0 4px #fff;
+	}
+
+	button {
+		all: unset;
+		display: inline-block;
+		margin-top: 2rem;
+		cursor: pointer;
+		background-color:  var(--primary-color);
+		font-size: 1.25rem;
+		font-weight: 600;
+		color: white;
+		padding: .75em 1.5em;
+		border-radius: 2em;
+		box-shadow: 0 5px 20px #4bcb8d66;
+		transition: opacity 1s, visibility 1s, transform .3s;
+	}
+
+	button:hover {
+		transform: scale(1.02);
+	}
+
+	.vergelijking .right {
+		content: '';
+		font-size: 2.5rem;
+		width: 2.5rem;
+		height: 2.5rem;
+		position: absolute;
+		bottom: 1.5rem;
+		right: 1.5rem;
+		z-index: 1000;
 	}
 </style>
--- green-caravan/src/routes/comparable.svelte
+++ green-caravan/src/routes/comparable.svelte
@@ -4,32 +4,65 @@
 </script>
 
 <div class="main">
-	<p>Hoeveel CO2 komt er vrij?</p>
-	<h1>Laadpaal</h1>
-	<p>Bij het opladen van je elektrische auto komen er fossiele brandstoffen vrij. Waar valt dat mee te vergelijken?</p>
+	<div class="bubble">
+		<h1>Hoeveel CO2 komt er vrij?</h1>
+		<p>Zelfs bij het opladen van je elektrische auto komt er CO<sub>2</sub> vrij. Swipe om vergelijkingen te zien.</p>
+	</div>
 	<section class="rain">
-		<Comparison itemAmount={1072} itemName={'sigaretten'} itemEmoji={'🚬'} />
-		<Comparison itemAmount={653} itemName={'theetjes'} itemEmoji={'🫖'} />
-		<Comparison itemAmount={180} itemName={'minuten douchen'} itemEmoji={'🚿'} />
-		<Comparison itemAmount={60} itemName={'km vliegen'} itemEmoji={'✈️'} />
-		<Comparison itemAmount={1500} itemName={'plastic tasjes'} itemEmoji={'🛍'} />
+		<Comparison itemAmount={1072} itemName={'sigaretten'} itemEmoji={'🚬'} nextPage={'🫖'}/>
+		<Comparison itemAmount={653} itemName={'theetjes'} itemEmoji={'🫖'} nextPage={'🚿'}/>
+		<Comparison itemAmount={180} itemName={'minuten douchen'} itemEmoji={'🚿'} nextPage={'✈️'}/>
+		<Comparison itemAmount={60} itemName={'km vliegen'} itemEmoji={'✈️'} nextPage={'🛍'}/>
+		<Comparison itemAmount={1500} itemName={'plastic tasjes'} itemEmoji={'🛍'} nextPage={'🚬'}/>
 	</section>
 </div>
 
 <style>
-	.main {
-		background-color: var(--primary-color);
+
+	.bubble {
+		text-align: center;
+		padding: 2rem 0;
+		position: relative;
+		overflow: hidden;
+		/* margin-bottom: 1.5rem; */
+		border-radius: 0.5rem;
+		padding-bottom: 3rem;
+		z-index: 1000;
+	}
+
+	.bubble::before {
+		position: absolute;
+		content: '';
+		width: 120%;
 		height: 100%;
-		padding-top: 3rem;
+		inset: 0 0 0 -10%;
+		background-color: var(--primary-color);
+		border-radius: 0 0 80% 80%;
+		z-index: -1;
+	}
+
+	.bubble * {
+		color: #fff;
+	}
+	.main {
+		/* background-color: var(--primary-color); */
+		height: 100%;
+		/* padding-top: 3rem; */
+		overflow-y: hidden;
 	}
 
 	.rain {
-		height: 78%;
-		margin-top: 1rem;
+		height: 37.7rem;
 		display: flex;
 		overflow-x: scroll;
 		scroll-snap-type: x mandatory;
+		scroll-behavior: smooth;
+		margin-top: -3rem;
 	}
+
+	.rain::-webkit-scrollbar {
+  display: none;
+}
 
 	h1,
 	p {
@@ -41,10 +74,11 @@
 
 	h1 {
 		margin: 0.5rem auto 0.75rem auto;
+		font-size: 1.5rem;
 	}
 
-	p:first-of-type {
+	/* p:first-of-type {
 		opacity: 0.6;
 		font-size: 18px;
-	}
+	} */
 </style>
